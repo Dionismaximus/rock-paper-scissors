dea5204af588b1668a63330f60064b1017291a21
Denis Litvinenko <dionismaximus666@gmail.com>
2024-04-07 17:59:40 +0000

    Add function playGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let choices = ['rock', 'paper', 'scissors']
+let aiImageChoice = document.getElementById('ai-image-choice')
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName('button')
 
@@ -6,9 +9,20 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute('data-type')) {
                 let userChoice = this.getAttribute('data-type')
                 alert(`You choose ${userChoice}`)
+                playGame(userChoice);
             } else {
                 console.log('Wrong choice')
             }
         })
     }
 })
+
+
+function playGame(userChoice) {
+
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    alert(`Computer choice ${computerChoice}`)
+    aiImageChoice.src = `assets/images/${computerChoice}.jpg`
+    alert(`Image for AI is ${computerChoice}`)
+
+}
