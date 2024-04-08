0bb5f5adfa9a95a335404ec9743d0389b68c487b
Denis Litvinenko <dionismaximus666@gmail.com>
2024-04-08 11:39:28 +0000

    Add determineWinner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 let choices = ['rock', 'paper', 'scissors']
 let aiImageChoice = document.getElementById('ai-image-choice')
+let computerChoice = '';
+let userChoice =''
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName('button')
@@ -7,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type')) {
-                let userChoice = this.getAttribute('data-type')
+                userChoice = this.getAttribute('data-type')
                 alert(`You choose ${userChoice}`)
                 playGame(userChoice);
             } else {
@@ -20,7 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function playGame(userChoice) {
 
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    computerChoice = choices[Math.floor(Math.random() * choices.length)]
     
     aiImageChoice.src = `assets/images/${computerChoice}.jpg`
+    determineWinner();
+}
+
+function determineWinner() {
+    if (userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'paper' && computerChoice ==='rock' || userChoice === 'scissors' && computerChoice === 'paper') {
+        alert(`You win ${userChoice}`)
+        console.log('You win')
+    } else if (userChoice === computerChoice) {
+        alert(`Draw`)
+    } else {
+        alert(`Computer win ${computerChoice}`)
+    }
 }
